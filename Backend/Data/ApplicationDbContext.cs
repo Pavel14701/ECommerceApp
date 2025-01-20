9776752a36e386918f7dec94bff32a8d417328b1
Pavel14701 <pavelkutia@gmail.com>
2025-01-21 00:58:18 +0300

    update ApplicationDbContext

--- Backend/Data/ApplicationDbContext.cs
+++ Backend/Data/ApplicationDbContext.cs
@@ -6,6 +6,38 @@ public class ApplicationDbContext : DbContext
     public DbSet<Product> Products { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<News> News { get; set; }
+    public DbSet<Images> Images { get; set; }
+    public DbSet<Content> Contents { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>()
+            .HasMany(p => p.Images)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<News>()
+            .HasMany(n => n.Images)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<News>()
+            .HasMany(n => n.Content)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Order>()
+            .HasMany(o => o.Items)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Order>()
+            .HasMany(o => o.Discounts)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
